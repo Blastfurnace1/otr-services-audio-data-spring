beb47f3dec1a2fa57b0b5930874c5ee50c8f4218
James B Blackson <jamesblackson880@gmail.com>
2018-07-05 21:33:31 -0400

    fix parent class

--- src/test/java/com/blastfurnace/otr/audio/AudioServiceApplicationTests.java
+++ src/test/java/com/blastfurnace/otr/audio/AudioServiceApplicationTests.java
@@ -5,12 +5,8 @@ package com.blastfurnace.otr.audio;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
-import com.blastfurnace.otr.utils.UtilitiesApplicationTest;
+import com.blastfurnace.otr.AppConfigTest;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.Assert.assertTrue;
@@ -19,31 +15,14 @@ import static org.junit.Assert.assertTrue;
  * Integration Tests for Audio Services
  *
  * @author Jim Blackson*/
-public class AudioServiceApplicationTests extends UtilitiesApplicationTest {
+public class AudioServiceApplicationTests extends AppConfigTest {
 
 	private static final Logger log = LoggerFactory.getLogger(AudioServiceApplicationTests.class); 
 	
 	@Test
 	public void shouldPerformAudioServiceTests() throws Exception {
 		log.info("Audio Service Tests - Start");
-//		@SuppressWarnings("rawtypes")
-//		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
-//				"http://localhost:" + this.port + "/rest/get/3", Map.class);
-//
-//		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//		
-//		HttpHeaders httpHeaders = this.testRestTemplate
-//				  .headForHeaders("http://localhost:" + this.port + "/rest/get/3");
-//		
-//		assertTrue(httpHeaders.getContentType()
-//				  .includes(MediaType.APPLICATION_JSON));
-//		
-//		AudioFileProperties audio = this.testRestTemplate.getForObject(
-//				"http://localhost:" + this.port + "/rest/get/3", AudioFileProperties.class);
-//
-//		then(entity).isNotNull();
-//		then(audio.getId()).isEqualTo(3);
-//		then(audio.getFilename()).isNotNull();
+
 		log.info("Audio Service Tests - End");
 	}
 	
